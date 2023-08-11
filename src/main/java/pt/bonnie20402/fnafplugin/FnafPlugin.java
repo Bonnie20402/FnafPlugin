f84a150abb1fdf9ddf4605059ec138d5be80d647
bonnie20402 <darioperreira2013@gmail.com>
2023-08-11 16:46:07 +0100

    Initial commit

---
+++ src/main/java/pt/bonnie20402/fnafplugin/FnafPlugin.java
@@ -0,0 +1,17 @@
+package pt.bonnie20402.fnafplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FnafPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
